e5cade33597ae4816a39e8f8ef81ecf6c4d10485
Diegogonzalz <103603185+Diegogonzalz@users.noreply.github.com>
2022-05-04 19:46:31 -0400

    Update ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -5,7 +5,7 @@
 #2 decimales
 cantidad_invertir=float(input("Cantidad a invertir (U$): "))
 interes_anual=float(input("Interes anual (%): " ))
-num_años=float(input("Cantidad de años a invertir: "))
+num_años=int(input("Cantidad de años a invertir: "))
 capital_ob=((cantidad_invertir*(((interes_anual/100)+1))**num_años))
 capital_ob=round(capital_ob,2)
 capital_ob=str(capital_ob)
